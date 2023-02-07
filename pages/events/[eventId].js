27aff777cc9a51e3eadbfad91a350a1ac2be1205
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-07 13:10:57 +0200

    event detail

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,26 @@
+import EventContent from "@/components/eventDetail/EventContent";
+import EventLogistics from "@/components/eventDetail/EventLogistics";
+import EventSummary from "@/components/eventDetail/EventSummary";
+import { getEventById } from "@/data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+  if (!event) return <p>No event found!</p>;
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
   );
 };
 
