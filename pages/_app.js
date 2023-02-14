aa3a3eb93448561a3acd6301f50f84a1c6b2fd74
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-14 05:07:12 +0200

    Head content

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import Layout from "@/components/layout/Layout";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        {/** This is the default title unless overwritten */}
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
